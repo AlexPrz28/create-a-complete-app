264db9d59a2a67e3f04f94c1393860f77ec38584
Alejandro Perez <alex98.perez@hotmail.com>
2021-05-07 19:09:45 -0600

    challenge 4

--- BookStore/AddViewController.swift
+++ BookStore/AddViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AddViewController: UIViewController, UITextFieldDelegate {
+class AddViewController: UIViewController, UITextFieldDelegate,  UITextViewDelegate {
     
     @IBOutlet weak var titleText: UITextField!
     @IBOutlet weak var authorText: UITextField!
@@ -49,6 +49,7 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         titleText.delegate = self
         authorText.delegate = self
         pagesText.delegate = self
+        descriptionText.delegate = self
         if editBook {
             print("mostrando cosas")
             self.title = "Edit Book"
@@ -72,5 +73,16 @@ class AddViewController: UIViewController, UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        else{
+            return true
+        }
+        
     }
 }
